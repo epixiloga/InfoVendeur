1eb1965bee66ae45056787c94cd64c01fb8a34b0
epixiloga <hafbenmoussa@gmail.com>
2018-12-22 22:02:44 +0100

    Ajoutez des fichiers projet.

---
+++ BossMvcVendeur/Controllers/VendeurController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BossMvcVendeur.Controllers
+{
+    public class VendeurController : Controller
+    {
+        public ActionResult Show(FormCollection collection)
+        {
+            if(collection["choixVendeur"] != null && collection["choixVendeur"] != "default")
+            {
+                this.ViewBag.VendeurId = int.Parse(collection["choixVendeur"]);
+            }
+
+            this.ViewBag.Vendeurs = Layers.VendeurLayer.GetAll(); // On récupère la liste de tous les vendeurs pour les afficher sur la liste
+            if(this.ViewBag.VendeurId != null)
+            {
+                this.ViewBag.Vendeur = Layers.VendeurLayer.GetById(this.ViewBag.VendeurId); // On récupère le vendeur par Id pour l'afficher (avec ses photos)
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BossMvcVendeur/Layers/VendeurLayer.cs
@@ -0,0 +1,34 @@
+﻿using BossMvcVendeur.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BossMvcVendeur.Layers
+{
+    public class VendeurLayer
+    {
+        static public List<Vendeur> GetAll()
+        {
+            using (KartinaEntities context=new KartinaEntities())
+            {
+                var query = from v in context.Vendeur
+                            select v;
+                return query.ToList();
+
+            }
+        }
+
+        static public Vendeur GetById(int id)
+        {
+            using (KartinaEntities context = new KartinaEntities())
+            {
+                var query = from v in context.Vendeur.Include("Photo")
+                            where v.Id == id
+                            select v;
+
+                return query.FirstOrDefault();
+            }
+        }
+    }
+}
